a8dbba6810118f960cb60ece8d6b0153b2b3906b
Moises Alejandro Alvarado Claro <moises.alvarado@globant.com>
2019-02-27 17:46:12 -0600

    using the common matchers

---
+++ tests/matchers.test.js
@@ -0,0 +1,24 @@
+describe('Usando comparadores comunes: ', () => {
+    const user = {
+        name: "Moisés",
+        lastname: "Alvarado",
+    }
+
+    const user2 = {
+        name: "Moisés",
+        lastname: "Alvarado",
+    }
+
+    const otherUser = {
+        name: "Oscar",
+        lastname: "Castro",
+    }
+
+    test('Igualdad de elementos', () => {
+        expect(user).toEqual(user2);
+    });
+
+    test('No igualdad de elementos', () => {
+        expect(user).not.toEqual(otherUser);
+    })
+});
