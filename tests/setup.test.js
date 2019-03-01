1ab97d58d7f64870bb3bb44e0401a8b03e062f94
Moises Alejandro Alvarado Claro <moises.alvarado@globant.com>
2019-03-01 16:25:23 -0600

    Testing before and after functions

---
+++ tests/setup.test.js
@@ -0,0 +1,15 @@
+// Despues de cada prueba:
+afterEach(() => console.info('Despues de la prueba.'));
+// Despues de cada prueba:
+afterAll(() => console.info('Despues de todas pruebas.'));
+
+// Antes de cada prueba:
+beforeEach(() => console.info('Antes de cada prueba.'))
+// Antes de todas las pruebas:
+beforeAll(() => console.info('Antes de todas las pruebas'));
+
+describe('preparar antes de ejecutar', () => {
+    test('es verdadero', () => {
+        expect(false).toBeFalsy();
+    })
+});
