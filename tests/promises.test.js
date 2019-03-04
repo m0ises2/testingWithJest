d2f8fa063ab0083de0ee651cfabe99a8b7b554a1
Moises Alejandro Alvarado Claro <moises.alvarado@globant.com>
2019-03-04 17:40:17 -0600

    testing out rejects and resolves promises

--- tests/promises.test.js
+++ tests/promises.test.js
@@ -5,9 +5,17 @@ const url_e = 'https://rickandmortyapi.com/api/character';
 describe('Probando promeses', () => {
     test('Realizando una petición a una API', done => {
         getDataFromApi(url_e).then(data => {
-            console.log('%o', data);
             expect(data.data.results.length).toBeGreaterThan(0);
             done();
         });
     });
-})
+
+    test('Resuelve hola', () => {
+        return expect(Promise.resolve('Hola!')).resolves.toBe('Hola!');
+    });
+
+    test('Resuelve con un error', () =>{
+        return expect(Promise.reject('Error')).rejects.toBe('Error');
+    });
+
+});
